c22d8d54a3318301265fbb04cbc55db7a8b43a5f
Chizzy Ibemeru <ibemeruchizzy@gmail.com>
2021-09-19 00:56:38 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+//Selectors
+
+const root = document.querySelector (':root');
+const container = document.querySelector ('.container');
+const todoContainer = document.querySelector ('.todo-container');
+const todoList = document.querySelector ('.todo-list');
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+
+//Event listener
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteComplete);
+todoInput.addEventListener('click', editInput)
+
+//Functions
+
+function addTodo(event) {
+    //event.preventDefault prevents form from submitting
+    event.preventDefault();
+    //Create Todo DIV and its classes
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //Create LI
+    const newTodo = document.createElement('li');
+    newTodo.classList.add('todo-item');
+    //When we type in the input and submit, it should reflect the value of what we typed
+    newTodo.textContent = todoInput.value;
+    //Appending the todo-item into the todoDiv created above
+    todoDiv.appendChild(newTodo);
+    //Creating a Completed Button
+    const completedButton = document.createElement('button');
+    //Using innerHTML to create a class so as to display the check button
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+     //Creating a Trash Button
+     const trashButton = document.createElement('button');
+     //Using innerHTML to create a class so as to display the trash button
+     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+     trashButton.classList.add('trash-btn');
+     todoDiv.appendChild(trashButton);
+     //Appending the newly created todoDiv to the TASKSLIST
+     todoList.appendChild(todoDiv);
+     //Create an edit button
+    const editButton = document.createElement('button');
+    editButton.textContent = 'edit'
+    editButton.classList.add('editButton');
+    todoDiv.appendChild(editButton)
+     //Clear todo INPUT VALUE
+     todoInput.value= '';
+     //Edit todo Input value
+     
+}
+
+
+function deleteComplete (e) {
+    const item = e.target;
+    //Delete Todo
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        todo.remove();
+    }
+    //Complete Todo
+    if(item.classList[0] === 'complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
+}
+
+function editInput (e) { e = e || window.event;
+    var target = e.target || e.srcElement;
+
+
+    console.log(target)
+
+}
